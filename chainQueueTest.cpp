7ff2b19a38620b5fab9dfa4ac05a51b5f36ac490
ShroXd <ShroXd@gmail.com>
2020-06-01 15:34:40 +0800

    test (chainQueueTest): 为链表实现的队列添加测试

---
+++ chainQueueTest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "./queue/chainQueue.h"
+
+using namespace std;
+
+int main()
+{
+    chainQueue<int> s;
+
+    if (s.empty())
+    {
+        cout << "The queue is empty." << endl;
+    }
+
+    s.push(100);
+    s.push(200);
+    s.push(300);
+
+    cout << s;
+
+    s.pop();
+    cout << s;
+
+    cout << "The front of queue is: " << s.front() << endl;
+    cout << "The back of queue is: " << s.back() << endl;
+}
