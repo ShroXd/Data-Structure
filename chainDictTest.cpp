0e6af70e5b99612035916b5bc3158d7f5bfddbd7
ShroXd <ShroXd@gmail.com>
2020-06-03 11:49:17 +0800

    test (chainDictTest): 为链表实现的字典增加了测试

---
+++ chainDictTest.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "./dictionary/chainDict.h"
+
+using namespace std;
+
+int main()
+{
+    chainDict<string, int> s;
+    pair<string, int> p;
+
+    p.first = "spike";
+    p.second = 24;
+    s.insert(p);
+
+    p.first = "hesea";
+    p.second = 26;
+    s.insert(p);
+
+    p.first = "Duch";
+    p.second = 29;
+    s.insert(p);
+
+    cout << s;
+
+    cout << "The age of spike is: " << s.find("spike")->second << endl;
+
+    s.erase("spike");
+    cout << s;
+}
