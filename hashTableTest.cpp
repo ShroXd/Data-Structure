3888dc05e80aac0bb65b27cddd87d1e5ab28197b
ShroXd <ShroXd@gmail.com>
2020-06-04 18:19:10 +0800

    test (hashTableTest): 为哈希表的实现增加了测试

---
+++ hashTableTest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "./dictionary/hashTable.h"
+
+using namespace std;
+
+int main()
+{
+    hashTable<string, int> s;
+    pair<string, int> p;
+
+    p.first = "spike";
+    p.second = 24;
+    s.insert(p);
+
+    p.first = "Hesea";
+    p.second = 26;
+    s.insert(p);
+
+    p.first = "Duch";
+    p.second = 26;
+    s.insert(p);
+
+    s.erase("spike");
+    s.erase("Hesea");
+    s.erase("Duch");
+    cout << s;
+}
