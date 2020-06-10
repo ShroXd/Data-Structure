a860dc914980d09bd502360debaf859e678de29a
ShroXd <ShroXdF@gmail.com>
2020-06-10 15:57:31 +0800

    test (binaryTreeTest): 为二叉树前序遍历增加测试

---
+++ binaryTreeTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "./binaryTree/linkedBinaryTree.h"
+
+using namespace std;
+
+int main()
+{
+    linkedBinaryTree<int> x, y, z, leave;
+
+    x.makeTree(1, leave, leave);
+    y.makeTree(2, x, leave);
+    z.makeTree(3, x, y);
+
+    z.preOrderOutput();     // 3 2 1
+}
