8b3d67709d43e8ca12219a6ec9c3cca75a5fd254
ShroXd <ShroXdF@gmail.com>
2020-06-15 15:17:32 +0800

    test (maxHeapTest):  为大根堆充实测试

--- maxHeap/test/maxHeapTest.cpp
+++ maxHeap/test/maxHeapTest.cpp
@@ -5,10 +5,33 @@ using namespace std;
 
 int main()
 {
-    maxHeap<int> s;
-    s.push(10);
-    s.push(20);
-    s.push(30);
+    maxHeap<int> s, h;
+    for (int i = 1; i < 10; i++)
+    {
+        s.push(i * 10);
+    }
 
     cout << s;
+
+    s.pop();
+    s.pop();
+    cout << s;
+    s.pop();
+    cout << s;
+    s.pop();
+    cout << s;
+    s.pop();
+    s.pop();
+    s.pop();
+
+    cout << s;
+
+    int z[10];
+    for (int i = 1; i < 10; i++)
+    {
+        cout << "? " << i << ' ' << endl;
+        z[i] = i;
+    }
+    h.initialize(z, 9);
+    cout << h;
 }
